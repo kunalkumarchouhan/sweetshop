30ae8594d3ee34eab5af81218c0c1b9766efaa90
Anshuman Gupta <anshuman844@gmail.com>
2020-08-06 22:24:15 +0530

    feat: Add styled files

---
+++ src/styled/variables.js
@@ -0,0 +1,17 @@
+export const breakPoints = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+   tablet: '768px',
+  laptopS: '1024px',
+  laptopM: '1440px',
+    fourK: '2560px'
+};
+
+export const fonts = [
+  `'Bitter', serif`,
+  `'Roboto', sans-serif`,
+  `'Raleway', sans-serif`,
+  `'Pacifico', cursive`,
+  `'Source Code Pro', monospace`
+];
